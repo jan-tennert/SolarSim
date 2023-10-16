d60d3bd9c8fdc8eddb13c46140a14facd62757a6
TheRealJan <jan.m.tennert@gmail.com>
2023-10-16 10:49:03 +0200

    fix holes in orbit lines and make them fps independent

--- src/orbit_lines.rs
+++ src/orbit_lines.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::{Plugin, App, Resource, Res, Entity, Query, Transform, Update, IntoSystemConfigs, in_state, Vec3, Material, Color, Commands, MaterialMeshBundle, default, Assets, Gizmos, With, Without}, time::{Timer, TimerMode, Time}, render::{render_resource::{PrimitiveTopology, RenderPipelineDescriptor, SpecializedMeshPipelineError, PolygonMode, ShaderRef, AsBindGroup}, mesh::MeshVertexBufferLayout}, pbr::{MaterialPipeline, MaterialPipelineKey}, reflect::{TypePath, TypeUuid}};
+use bevy::{prelude::{Plugin, App, Resource, Res, Entity, Query, Transform, Update, IntoSystemConfigs, in_state, Vec3, Material, Color, Commands, MaterialMeshBundle, default, Assets, Gizmos, With, Without, ResMut}, time::{Timer, TimerMode, Time}, render::{render_resource::{PrimitiveTopology, RenderPipelineDescriptor, SpecializedMeshPipelineError, PolygonMode, ShaderRef, AsBindGroup}, mesh::MeshVertexBufferLayout}, pbr::{MaterialPipeline, MaterialPipelineKey}, reflect::{TypePath, TypeUuid}};
 
 use crate::{body::{OrbitSettings, Planet, Moon, Star, BodyChildren, SimPosition}, SimState, physics::update_position, selection::SelectedEntity, constants::M_TO_UNIT};
 
@@ -8,6 +8,7 @@ impl Plugin for OrbitLinePlugin {
     fn build(&self, app: &mut App) {
         app
             .init_resource::<OrbitOffset>()
+            .init_resource::<OffsetTimer>()
             .add_systems(Update, (update_lines.after(update_position), draw_orbit_line.after(update_lines), clean_orbit_lines.after(draw_orbit_line)).run_if(in_state(SimState::Simulation)));
     }
 }
@@ -15,23 +16,38 @@ impl Plugin for OrbitLinePlugin {
 #[derive(Resource, Default)]
 pub struct OrbitOffset(pub Vec3);
 
+#[derive(Resource)]
+struct OffsetTimer(Timer);
+
+impl Default for OffsetTimer {
+    fn default() -> Self {
+        Self(Timer::from_seconds(0.005, TimerMode::Repeating))
+    }
+}
+
 fn update_lines(
     mut planet_query: Query<(&mut OrbitSettings, &SimPosition, &BodyChildren, With<Planet>, Without<Moon>, Without<Star>)>,
     mut moon_query: Query<(Entity, &SimPosition, &mut OrbitSettings, With<Moon>, Without<Planet>, Without<Star>)>,
+    time: Res<Time>,
+    mut timer: ResMut<OffsetTimer>
 ) {
-    for (mut orbit, pos, _, _, _, _) in &mut planet_query {
-        if orbit.draw_lines {
-            orbit.lines.push((pos.0 * M_TO_UNIT).as_vec3());
+    timer.0.tick(time.delta());
+    
+    if timer.0.finished() {
+        for (mut orbit, pos, _, _, _, _) in &mut planet_query {
+            if orbit.draw_lines {
+                orbit.lines.push((pos.0 * M_TO_UNIT).as_vec3());
+            }
         }
-    }
-    for (entity, pos, mut orbit, _, _, _) in &mut moon_query {
-        if orbit.draw_lines {
-            if let Some((_, p_pos, _, _, _, _)) = planet_query.iter().find(|(_, _, children, _, _, _)| {
-                children.0.contains(&entity)
-            }) {
-                let raw_p_pos = (p_pos.0 * M_TO_UNIT).as_vec3();
-                let raw_pos = (pos.0 * M_TO_UNIT).as_vec3();
-                orbit.lines.push(raw_pos - raw_p_pos);
+        for (entity, pos, mut orbit, _, _, _) in &mut moon_query {
+            if orbit.draw_lines {
+                if let Some((_, p_pos, _, _, _, _)) = planet_query.iter().find(|(_, _, children, _, _, _)| {
+                    children.0.contains(&entity)
+                }) {
+                    let raw_p_pos = (p_pos.0 * M_TO_UNIT).as_vec3();
+                    let raw_pos = (pos.0 * M_TO_UNIT).as_vec3();
+                    orbit.lines.push(raw_pos - raw_p_pos);
+                }
             }
         }
     }
@@ -61,12 +77,9 @@ fn draw_orbit_line(
 }
 
 pub fn draw_lines(orbit: &OrbitSettings, offset: Vec3, gizmos: &mut Gizmos) {
-    let points: Vec<&[Vec3]> = orbit.lines.chunks(2).collect();
-    for outer in points {
-        if let Some(first) = outer.get(0) {
-            if let Some(second) = outer.get(1) {
-                gizmos.line(*first + offset, *second + offset, orbit.color);
-            }
+    for (index, first) in orbit.lines.iter().enumerate() {
+        if let Some(second) = orbit.lines.get(index + 1) {
+            gizmos.line(*first + offset, *second + offset, orbit.color);
         }
     }
 }
